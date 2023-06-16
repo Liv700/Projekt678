c27f4d2d3d4a055b4b4f2a84feffebf01d504f5d
Liv700 <liwia.sikorska@gmail.com>
2023-06-16 14:41:05 +0200

    Zmodyfikowano program, tak a by zapis i odczyt z pliku odbywał się asynchronicznie oraz dodano funkcje wpisywania nazwy drugiego pliku

--- Desktop/Program/program.py
+++ Desktop/Program/program.py
@@ -3,11 +3,13 @@ import yaml
 import xmltodict
 import sys
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
+import threading
+import os
 
 def parsuj_argumenty():
     if len(sys.argv) != 3:
-        print("Sposób użycia: program.exe pathFile1.x pathFile2.y")
+        print("Sposób użycia: program.py pathFile1.x pathFile2.y")
         sys.exit(1)
 
     file1 = sys.argv[1]
@@ -20,19 +22,19 @@ def wczytaj_json(file):
             data = json.load(f)
         return data
     except FileNotFoundError:
-        print(f"Plik '{file}' nie istnieje.")
+        messagebox.showerror("Błąd", f"Plik '{file}' nie istnieje.")
         sys.exit(1)
     except json.JSONDecodeError:
-        print(f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni JSON.")
+        messagebox.showerror("Błąd", f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni JSON.")
         sys.exit(1)
 
 def zapisz_json(data, file):
     try:
         with open(file, 'w') as f:
             json.dump(data, f, indent=4)
-        print(f"Dane zapisane do pliku '{file}' w formacie JSON.")
+        messagebox.showinfo("Sukces", f"Dane zapisane do pliku '{file}' w formacie JSON.")
     except:
-        print(f"Błąd podczas zapisu danych do pliku '{file}'.")
+        messagebox.showerror("Błąd", f"Błąd podczas zapisu danych do pliku '{file}'.")
 
 def wczytaj_yaml(file):
     try:
@@ -40,19 +42,19 @@ def wczytaj_yaml(file):
             data = yaml.safe_load(f)
         return data
     except FileNotFoundError:
-        print(f"Plik '{file}' nie istnieje.")
+        messagebox.showerror("Błąd", f"Plik '{file}' nie istnieje.")
         sys.exit(1)
     except yaml.YAMLError:
-        print(f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni YAML.")
+        messagebox.showerror("Błąd", f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni YAML.")
         sys.exit(1)
 
 def zapis_yaml(data, file):
     try:
         with open(file, 'w') as f:
             yaml.dump(data, f)
-        print(f"Dane zapisane do pliku '{file}' w formacie YAML.")
+        messagebox.showinfo("Sukces", f"Dane zapisane do pliku '{file}' w formacie YAML.")
     except:
-        print(f"Błąd podczas zapisu danych do pliku '{file}'.")
+        messagebox.showerror("Błąd", f"Błąd podczas zapisu danych do pliku '{file}'.")
 
 def wczytaj_xml(file):
     try:
@@ -60,10 +62,10 @@ def wczytaj_xml(file):
             data = xmltodict.parse(f.read())
         return data
     except FileNotFoundError:
-        print(f"Plik '{file}' nie istnieje.")
+        messagebox.showerror("Błąd", f"Plik '{file}' nie istnieje.")
         sys.exit(1)
     except xmltodict.ParseError:
-        print(f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni XML.")
+        messagebox.showerror("Błąd", f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni XML.")
         sys.exit(1)
 
 def zapisz_xml(data, file):
@@ -71,54 +73,53 @@ def zapisz_xml(data, file):
         with open(file, 'w') as f:
             xml_str = xmltodict.unparse({'root': data}, pretty=True)
             f.write(xml_str)
-        print(f"Dane zapisane do pliku '{file}' w formacie XML.")
+        messagebox.showinfo("Sukces", f"Dane zapisane do pliku '{file}' w formacie XML.")
     except:
-        print(f"Błąd podczas zapisu danych do pliku '{file}'.")
+        messagebox.showerror("Błąd", f"Błąd podczas zapisu danych do pliku '{file}'.")
 
 def sprawdz_format(plik):
     ext = plik.split('.')[-1].lower()
     if ext in ['json', 'yaml', 'yml', 'xml']:
         return ext
     else:
-        print(f"Nieobsługiwany format pliku '{plik}'.")
+        messagebox.showerror("Błąd", f"Nieobsługiwany format pliku '{plik}'.")
         sys.exit(1)
+
+def wczytaj_asynchronicznie(file, format):
+    if format == 'json':
+        return wczytaj_json(file)
+    elif format in ['yaml', 'yml']:
+        return wczytaj_yaml(file)
+    elif format == 'xml':
+        return wczytaj_xml(file)
+
+def zapisz_asynchronicznie(data, file, format):
+    if format == 'json':
+        zapisz_json(data, file)
+    elif format in ['yaml', 'yml']:
+        zapisz_yaml(data, file)
+    elif format == 'xml':
+        zapisz_xml(data, file)
 
 def przetworz_pliki():
     file1 = filedialog.askopenfilename()
-    file2 = filedialog.askopenfilename()
+    new_file = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON", "*.json"), ("YAML", "*.yaml"), ("XML", "*.xml")])
 
     ext1 = sprawdz_format(file1)
-    ext2 = sprawdz_format(file2)
+    ext2 = new_file.split('.')[-1].lower()
 
     if ext1 == ext2:
-        print("Formaty plików są identyczne. Brak konieczności konwersji.")
-    elif ext1 == 'json' and ext2 == 'yaml':
-        data1 = wczytaj_json(file1)
-        zapis_yaml(data1, file2)
-    elif ext1 == 'json' and ext2 == 'xml':
-        data1 = wczytaj_json(file1)
-        zapisz_xml(data1, file2)
-    elif ext1 == 'yaml' and ext2 == 'json':
-        data1 = wczytaj_yaml(file1)
-        zapisz_json(data1, file2)
-    elif ext1 == 'yaml' and ext2 == 'xml':
-        data1 = wczytaj_yaml(file1)
-        zapisz_xml(data1, file2)
-    elif ext1 == 'xml' and ext2 == 'json':
-        data1 = wczytaj_xml(file1)
-        zapisz_json(data1, file2)
-    elif ext1 == 'xml' and ext2 == 'yaml':
-        data1 = wczytaj_xml(file1)
-        zapis_yaml(data1, file2)
+        messagebox.showinfo("Informacja", "Formaty plików są identyczne. Brak konieczności konwersji.")
     else:
-        print("Nieobsługiwane kombinacje formatów plików.")
+        data1 = wczytaj_asynchronicznie(file1, ext1)
+        t = threading.Thread(target=zapisz_asynchronicznie, args=(data1, new_file, ext2))
+        t.start()
 
 def main():
     root = tk.Tk()
-    root.withdraw()  
+    root.withdraw()
 
     przetworz_pliki()
 
 if __name__ == '__main__':
     main()
-
